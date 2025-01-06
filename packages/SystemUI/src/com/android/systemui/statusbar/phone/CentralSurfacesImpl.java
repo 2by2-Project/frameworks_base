7be9ac00a8ec2c3c24c9b8c7bfc3176df7299aee
Anna-Sophie Kasierocka <hanna.zaitsewa@gmail.com>
2025-01-06 21:40:25 +0100

    Ongoing action progressbar chip implementation
    This commit provides an implementation of a progressbar chip in statusbar which features ongoing action progress from a notification
    The chip is featured to the right of default statusbar clock position, but before the icon container. The contents of chip are miniature app icon and a progress bar
    The implementation provided is basic and lacks customizations which are planned to be added further
    Idea is taken from this reddit post: https://www.reddit.com/r/NOTHING/comments/1dyuak6/nothing_os_30_images_fan_made/
    
    Change-Id: Ide3ff14927b314d9b988e2c24eba5345fad168aa

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -208,12 +208,14 @@ import com.android.systemui.statusbar.KeyguardIndicationController;
 import com.android.systemui.statusbar.LiftReveal;
 import com.android.systemui.statusbar.LightRevealScrim;
 import com.android.systemui.statusbar.LockscreenShadeTransitionController;
+import com.android.systemui.statusbar.NotificationListener;
 import com.android.systemui.statusbar.NotificationLockscreenUserManager;
 import com.android.systemui.statusbar.NotificationMediaManager;
 import com.android.systemui.statusbar.NotificationPresenter;
 import com.android.systemui.statusbar.NotificationRemoteInputManager;
 import com.android.systemui.statusbar.NotificationShadeDepthController;
 import com.android.systemui.statusbar.NotificationShadeWindowController;
+import com.android.systemui.statusbar.OnGoingActionProgressController;
 import com.android.systemui.statusbar.PowerButtonReveal;
 import com.android.systemui.statusbar.PulseExpansionHandler;
 import com.android.systemui.statusbar.StatusBarState;
@@ -406,6 +408,10 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     private final LightRevealScrim mLightRevealScrim;
     private WiredChargingRippleController mChargingRippleAnimationController;
     private PowerButtonReveal mPowerButtonReveal;
+
+    private OnGoingActionProgressController mOnGoingActionProgressController = null;
+
+    @Inject public NotificationListener mNotificationListener;
 
     /**
      * Whether we should delay the wakeup animation (which shows the notifications and moves the
@@ -1273,6 +1279,11 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                     checkBarModes();
                     mBurnInProtectionController.setPhoneStatusBarView(
                             mPhoneStatusBarViewController.getView());
+                    mOnGoingActionProgressController =
+                            new OnGoingActionProgressController(
+                                    mContext,
+                                    statusBarViewController.getOngoingActionProgressGroup(),
+                                    mNotificationListener);
                 });
         mStatusBarInitializer.initializeStatusBar();
 
