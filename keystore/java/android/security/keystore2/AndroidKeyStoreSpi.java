fb2c2a3dcac1b65ca93942ce929fd16c1dc020c9
kailua <kailua@project2by2.jp>
2025-09-26 02:34:48 +0900

    keystore2: Don't spoof locked state if keybox is empty
    * Fixes key attestation with TEE working device.
    * At least we can get basic integrity in some devices.
    
    Co-authored-by: Garry050 <Garry.windows8@gmail.com>

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -211,7 +211,8 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
         X509Certificate modLeaf = leaf;
         try {
             byte[] bytes = leaf.getEncoded();
-            if (bytes != null && bytes.length > 0) {
+            if (bytes != null && bytes.length > 0
+                && KeyProviderManager.isKeyboxAvailable()) {
                 int index = indexOf(bytes);
                 if (index != -1) {
                     bytes[index + 38] = 1;
